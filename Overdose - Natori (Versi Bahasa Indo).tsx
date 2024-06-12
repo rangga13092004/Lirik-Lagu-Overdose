95be402a7e7fd0f593ef7e08a9605ac4331f1077
Maulana Rangga Arrosyid <134927221+rangga13092004@users.noreply.github.com>
2024-06-12 16:21:35 +0700

    Add files via upload

---
+++ Overdose - Natori (Versi Bahasa Indo).tsx
@@ -0,0 +1,66 @@
+Berikut adalah lirik lagu "Overdose" versi Bahasa Indonesia, yang populer dibawakan oleh Natori:
+
+Natori - Overdose (Versi Bahasa Indonesia)**
+
+Jujur saja ku sadar hanya kan berakhir percuma,namun mengapa 
+Sesuatu yang terselip di tangan ini rasanya bagai cinta.
+Kata-kata yang miliki makna sejatinya tak sulit digoyahkan
+Bermain-main dengan khayalan sungguh bosan selalu membandingkan jawaban
+Namun begitu tiap dekat dengan kebenaran 
+
+Reff:
+Terlena lupakan segalanya
+Memungkiri perasaan yang sebenarnya
+Saat kita bertatap
+
+Overdose, denganmu kini lalui hari-hari 
+Bagai tak berdaya di keruhnya mimpi
+Overdose, denganmu kini muslihat yang manis
+Don't stop it music, darling
+
+(Musik)
+
+Jujur kau pun sadar hanya kan berakhir percuma, lalu mengapa
+Penuh kebimbangan seiring waktu yang telah berlalu lama
+Tiada guna membuat alasan lebih baik mulai berterus terang
+
+Reff:
+Dibanding menimbun selamanya
+Akhiri sekarang pun tak buruk juga
+Sebelum kian membusuk hingga meninggalkan luka lebih parah nantinya (Tetap Saja)
+Terbuai lupakan segalanya 
+Memungkiri kenyataan yang sebenarnya
+Saat kita bertatap
+
+Overdose, denganmu kini ku ingin mengerti
+Tepikanlah segala dusta yang ku benci
+Overdose, denganmu kini ku ingin pahami
+Two step from (hell) with me, darling
+
+Breakdown:
+(Musik)
+
+Bridge:
+Overdose, denganmu kini lalui hari-hari
+Bagai tak berdaya di keruhnya mimpi
+Overdose, denganmu kini muslihat yang manis
+Don't stop it music, darling
+
+Breakdown 2:
+(Musik)
+
+Reff:
+Overdose, denganmu kini tak mampu mengerti
+Dalam pusat segala dusta yang ku benci (Biarlah oh)
+Denganmu kini muslihat yang manis 
+Don't stop it music, darling
+
+Break:
+(Musik)
+
+-youtube.com/vradnyanaz
+-instagram.com/vradnyanaz
+-fb.me/vradnyanaz
+-trakteer.id/vradnyanaz
+(Thanks to: Seseorang)
+
